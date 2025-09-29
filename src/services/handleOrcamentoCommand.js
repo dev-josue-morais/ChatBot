ef45b03d4a461daf6451477c1d155e4837e00fae
JOSUE DE SOUZA MORAIS <dev.josue.morais@outlook.com>
2025-09-28 23:52:10 -0300

    Atualizar o handleOrcamentoCommand.js

--- src/services/handleOrcamentoCommand.js
+++ src/services/handleOrcamentoCommand.js
@@ -330,10 +330,11 @@ async function handleOrcamentoCommand(command, userPhone) {
 
     // Puppeteer
     const puppeteer = require("puppeteer");
-    const browser = await puppeteer.launch({
+
+const browser = await puppeteer.launch({
   headless: true,
-  args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  executablePath: '/usr/bin/google-chrome' // caminho do Chrome no servidor
+  executablePath: '/usr/bin/chromium', // aponta para o Chromium instalado
+  args: ['--no-sandbox', '--disable-setuid-sandbox']
 });
 
     const page = await browser.newPage();
