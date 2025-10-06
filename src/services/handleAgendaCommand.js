1a92d3eacf9124b3413b3214876b359b837b7985
dev-josue-morais <josue28jrd@hotmail.com>
2025-10-06 10:23:49 -0300

    fix agenda 2

--- src/services/handleAgendaCommand.js
+++ src/services/handleAgendaCommand.js
@@ -68,7 +68,7 @@ async function handleAgendaCommand(command, userPhone) {
         const { data, error } = await supabase
           .from('events')
           .update(updates)
-          .eq('event_numero', command.id)
+          .eq('event_numero', command.event_numero)
           .eq('user_telefone', userPhone)
           .select('event_numero, title, date');
 
