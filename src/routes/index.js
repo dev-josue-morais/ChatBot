d8fe018b059ef1f236a0572efac9ab49bbd518eb
dev-josue-morais <josue28jrd@hotmail.com>
2025-10-31 13:13:59 -0300

    0

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,10 @@ const router = express.Router();
 
 const renewTokenRoute = require('./renewTokenRoute');
 const webhookRoute = require('./webhookRoute');
-const keepAlive = require('./keepAlive');
 const mpRouter = require('./mpWebhook');
 
 router.post('/mp-webhook', mpRouter);
 router.use('/renew-token', renewTokenRoute);
 router.use('/webhook', webhookRoute);
-router.use('/live', keepAlive);
 
 module.exports = router;
