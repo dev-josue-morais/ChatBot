276ef8aca86414a9c967c077255025f79cb257d5
JOSUE DE SOUZA MORAIS <dev.josue.morais@outlook.com>
2025-11-05 15:03:51 -0300

    Atualizar o index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const renewTokenRoute = require('./renewTokenRoute');
 const webhookRoute = require('./webhookRoute');
 const mpRouter = require('./mpWebhook');
+const keepAliveRoute = require('./keepAliveRoute');
 
 router.post('/mp-webhook', mpRouter);
 router.use('/renew-token', renewTokenRoute);
 router.use('/webhook', webhookRoute);
+router.use('/keep-alive', keepAliveRoute)
 
 module.exports = router;
