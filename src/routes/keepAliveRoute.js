5a54e412bdf12c6992381e542039f95403d11cb4
JOSUE DE SOUZA MORAIS <dev.josue.morais@outlook.com>
2025-11-06 17:14:05 -0300

    Atualizar o keepAliveRoute.js

--- src/routes/keepAliveRoute.js
+++ src/routes/keepAliveRoute.js
@@ -1,23 +1,9 @@
+// routes/keepAlive.js
 const express = require('express');
 const router = express.Router();
-const supabase = require('../services/supabase');
 
-router.get('/', async (req, res, next) => {
-  try {
-    const { data, error } = await supabase
-      .from('keep_alive')
-      .select('status')
-      .limit(1);
-
-    if (error) {
-      console.error('Erro no keep-alive Supabase:', error);
-      return res.status(500).send('Erro no keep-alive');
-}
-
-    res.send('1');
-  } catch (err) {
-    next(err);
-  }
+router.get('/', (req, res) => {
+  res.status(200).send('OK');
 });
 
 module.exports = router;
