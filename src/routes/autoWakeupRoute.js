e630b9704be9357740d25964f55eef1d6d9f5f9b
Josue de Souza Morais <dev.josue.morais@outlook.com>
2026-09-25 23:59:53 -0300

    start7

--- src/routes/autoWakeupRoute.js
+++ src/routes/autoWakeupRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  console.log('🔄 Keep Alive recebido:', new Date().toISOString());
+  // console.log('🔄 Keep Alive recebido:', new Date().toISOString());
 
   res.status(200).json({
     success: true,
